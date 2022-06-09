a484cdde8fe8f7bea6d2433dc42019e97eabcbee
JoseCarlos <jose.robles242216@gmail.com>
2022-06-08 22:32:21 -0700

    Se agrega función de suma y almacenamiento de numeros en array para multiples procesos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 //VARIABLES
-const numeros = document.querySelector('.numbers');
+const btnNumero = document.querySelector('.numbers');
 const acciones = document.querySelector('.process');
 const pantalla = document.querySelector('.pantalla');
+let numeros = [];
 
 
 //ESCUCHAR EVENTOS
-numeros.addEventListener('click', imprimirNumeros);
+btnNumero.addEventListener('click', imprimirNumeros);
 
 //FUNCIONES
 //Obtener el valor del botón e imprime el resultado en pantalla.
@@ -16,11 +17,21 @@ function imprimirNumeros(e){
     if(fuera === ''){
         if(pantalla.textContent.length <= 7){
             pantalla.textContent+=numero;
+            // numeros.push(numero);
+            numeros=[...numeros,parseInt(numero)];
+            console.log(numeros)
+        
         }   
     }
 }
 
-//Suma las cantidades 
-function sumar(){
-
+//Sumar los números capturados en un array
+function suma(){
+    let sumando = 0;
+    let resultado = 0;
+    for(let i = 0;i<=numeros.length-1;i++){
+        resultado = sumando += numeros[i];
+    }
+    return resultado;
 }
+console.log(suma())
